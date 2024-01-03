251ef21949c9d9aaaedfc4d40b39871757bc5132
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-01-03 17:18:41 -0300

    animated sequence+parallel aula 42.

--- App.js
+++ App.js
@@ -9,6 +9,43 @@ export default class App extends Component {
       AltAnimada: new Animated.Value(50),
       OpacidadeAnimada: new Animated.Value(0)
     };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Animated.View style={{
+          width: this.state.LarAnimada,
+          height: this.state.AltAnimada,
+          backgroundColor: '#4169E1',
+          justifyContent: 'center',
+          opacity: this.state.OpacidadeAnimada
+        }}>
+
+          <Text style={{
+            color: '#FFF',
+            fontSize: 22,
+            textAlign: 'center'
+          }}> Carregando... </Text>
+        </Animated.View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+/**
+ *  // animação sequencial + paralela / sequence + parallel
     Animated.sequence([
       Animated.timing(
         this.state.OpacidadeAnimada,
@@ -52,38 +89,6 @@ export default class App extends Component {
         }
       )
     ]).start();
-        */
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-
-        <Animated.View style={{
-          width: this.state.LarAnimada,
-          height: this.state.AltAnimada,
-          backgroundColor: '#4169E1',
-          justifyContent: 'center',
-          opacity: this.state.OpacidadeAnimada
-        }}>
-
-          <Text style={{
-            color: '#FFF',
-            fontSize: 22,
-            textAlign: 'center'
-          }}> Carregando... </Text>
-        </Animated.View>
-
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ */
